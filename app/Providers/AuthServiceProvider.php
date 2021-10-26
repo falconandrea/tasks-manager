f84ec22da799c96a1ce7a0e9f39e3d595827352c
Andrea Falcon <923390-andrea.falcon@users.noreply.gitlab.com>
2021-10-26 14:50:01 +0200

    feat: add policies for customer and project for store actions

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Customer;
+use App\Models\Project;
+use App\Policies\CustomerPolicy;
+use App\Policies\ProjectPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -13,7 +17,8 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        Customer::class => CustomerPolicy::class,
+        Project::class => ProjectPolicy::class
     ];
 
     /**
