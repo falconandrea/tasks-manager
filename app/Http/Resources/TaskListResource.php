4612562d461d03c4d261aece314d10b3d4868f54
Andrea Falcon <923390-andrea.falcon@users.noreply.gitlab.com>
2021-10-26 10:54:20 +0200

    feat: add resources

---
+++ app/Http/Resources/TaskListResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TaskListResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'            => $this->id,
+            'name'          => $this->name,
+            'user'          => new UserResource($this->user),
+            'description'   => $this->description,
+            'project'       => new ProjectResource($this->project),
+            'status'        => $this->status,
+            'priority'      => $this->priority,
+            'created_at'    => $this->created_at->toDateTimeString(),
+            'updated_at'    => $this->updated_at->toDateTimeString(),
+        ];
+    }
+}
---
+++ app/Http/Resources/TaskResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TaskResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'status' => $this->status,
+            'priority' => $this->priority,
+            'user' => new UserResource($this->user),
+            'project' => new ProjectResource($this->project),
+            'created_at' => $this->created_at->toDateTimeString(),
+            'updated_at' => $this->updated_at->toDateTimeString(),
+        ];
+    }
+}
